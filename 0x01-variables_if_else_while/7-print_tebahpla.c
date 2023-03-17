98823bfd42fe0209291b33ade5997be66a0fc011
ascot20 <danielotchere18@gmail.com>
2023-03-17 07:32:32 -0700

    added script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+	putchar(r);
+	putchar9'\n');
+	return (0);
+}
