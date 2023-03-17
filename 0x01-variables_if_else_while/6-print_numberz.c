16b91c02d23dd114303791ffebe11fb8360953c7
ascot20 <danielotchere18@gmail.com>
2023-03-17 07:27:52 -0700

    added script

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	putchar(48 + n);
+	putchar('\n');
+	return (0);
+}
