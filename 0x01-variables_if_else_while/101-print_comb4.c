bdd8bda776ae41cf95262da9b9dc8eb8167fd8a0
ascot20 <danielotchere18@gmail.com>
2023-03-19 03:34:14 -0700

    added script

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			for (k = 2; k < 10; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(48 + i);
+					putchar(48 + j);
+					putchar(48 + k);
+					if (i == 7 && j == 8 && k == 9)
+						continue;
+					else{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
