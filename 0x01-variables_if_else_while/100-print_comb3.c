79ae262cc273006f93776548ba6c0593b68668f7
ascot20 <danielotchere18@gmail.com>
2023-03-19 02:45:11 -0700

    added script

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+		if (i < j)
+		{	
+		putchar(48 + i);
+		putchar(48 + j);
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
