0c43d2f42a567dc1d4f8f7ced529e4c2f91b7988
ascot20 <danielotchere18@gmail.com>
2023-03-21 09:27:12 -0700

    added a script

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @x: parameter
+ * Return: returns int
+ */
+int print_last_digit(int x)
+{
+	return (x % 10);
+
+}
