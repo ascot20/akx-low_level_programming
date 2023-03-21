dcd8a92f27cd82554d3bf4b48da2cf972a023cab
ascot20 <danielotchere18@gmail.com>
2023-03-21 08:57:19 -0700

    added a script

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n:parameter to be checked
+ * return: 1 if greater than 0 0 if is zero -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+
+}
