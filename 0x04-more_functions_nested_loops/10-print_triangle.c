c5d87cb2c3395029f2a960b97782a860f5014dd9
ascot20 <danielotchere18@gmail.com>
2023-03-23 13:37:33 -0700

    made update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -3,21 +3,23 @@
  * print_triangle- print triangle
  * @size:input
  */
-void print_trinagle(int size){
-    int space = size - 1;
+void print_trinagle(int size)
+{
+	int space = size - 1;
 
-    while(space >= 0){
-        int hash = size - space;
+	while(space >= 0)
+	{
+		int hash = size - space;
 
-        for (int i = space; i > 0; i--){
-            _putchar(' ');
-        }
-        for (int x = hash; x > 0; x--){
-            _putchar('#');
-        }
-        _putchar('\n');
-        space--;
-    }
-
-
+		for (int i = space; i > 0; i--)
+			_putchar(' ');
+	
+		for (int x = hash; x > 0; x--)
+			_putchar('#');
+	
+	_putchar('\n');
+	space--;
+	}
+	if (size < 0)
+		_putchar('\n');
 }
