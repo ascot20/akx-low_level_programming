0923d5a2c57f9fe3ec06431d0eec02be205cf256
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-03-23 20:21:07 +0000

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void printcar(int size){
+    int space = size - 1;
+    while(space >= 0){
+        int hash = size - space;
+
+        for (int i = space; i > 0; i--){
+            _putchar(' ');
+        }
+        for (int x = hash; x > 0; x--){
+            _putchar('#');
+        }
+        _putchar('\n');
+        space--;
+    }
+
+
+}
