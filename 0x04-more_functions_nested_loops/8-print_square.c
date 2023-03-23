5d93a112d78ac21d39a03d60dd9dfcb7078169a5
ascot20 <danielotchere18@gmail.com>
2023-03-23 07:25:41 -0700

    fixed bug

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,23 +1,26 @@
 #include "main.h"
 /**
  * print_square-prints a square followed by a line
+ *
  * @size:parameter input
  */
 void print_square(int size)
 {
+	int total = size
+
 	if (size <= 0)
 	{
 		size = 0;
 		_putchar('\n');
 	}
 
-	int total = size;
+
 	while (size > 0)
 	{
 		int w = total;
 		while (w > 0)
 		{
-			_putchar('#');
+			_putchar(35);
 			w--;
 		}
 		_putchar('\n');
