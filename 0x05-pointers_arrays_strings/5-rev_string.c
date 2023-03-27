851beb33ba619858c91cff3976dad14959de1b29
ascot20 <danielotchere18@gmail.com>
2023-03-27 11:16:25 -0700

    fixed bug

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,14 @@ void rev_string(char *s)
 	
 	while (s[len])
 		len++;
-	int i;
+	int i = 0;
 
-	for (i = 0; i < len / 2; i++)
+	while (i < len / 2)
 	{
 		int temp = s[i];
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = temp;
+		i++;
 	}
 	_putchar('\n');
 
