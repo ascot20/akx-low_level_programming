ef6c117a27eb4666dd21d6665edde21a1c699911
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-03-28 10:13:17 +0000

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,10 +16,9 @@ char random_char() {
 
 int main() {
     
-    srand(time(NULL));
-
-    
     char password[21];
+    
+    srand(time(NULL));
     for (int i = 0; i < 20; i++) {
         password[i] = random_char();
     }
