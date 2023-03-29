e3678a61cc551ac33a25323b301c434bed491cde
ascot20 <danielotchere18@gmail.com>
2023-03-29 03:00:29 -0700

    added file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat-concatenates two strings
+ * @dest:pointer to string
+ * @src:pointer to string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while(*src)
+	{
+		dest[i] = *src;
+		src++;
+		i++;
+	}
+	return (dest);
+}
