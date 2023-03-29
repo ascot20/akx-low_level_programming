4b726ab29aacc94a34eed380039c174f98285e07
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-03-29 13:38:54 +0000

    Create 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strcmp-compares two strings
+* @s1:pointer to string
+* @s2:pointer to string
+* Return:return integer
+*/
+int _strcmp(char *s1, char *s2)
+{
+  int i = 0;
+  while (s1[i] == s2[i])
+  {
+     if (s1[i] == '\0')
+         return (0);
+     i++;
+  }
+    return (s1[i] - s2[i]);
+}
