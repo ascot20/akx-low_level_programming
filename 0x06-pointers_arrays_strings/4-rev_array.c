d97c395084e3e5c05c3bc0d6f61596bcfdb965eb
ascot20 <danielotchere18@gmail.com>
2023-03-30 01:46:18 -0700

    fixed requirement

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,11 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int j;
+	int temp;
 
 	for (i = 0, j = n - 1; i < j; i++, j--)
 	{
-		int temp = a[i];
+		temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
