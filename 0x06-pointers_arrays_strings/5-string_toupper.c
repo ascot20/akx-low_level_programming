bef0f31559d5c59c5f375eaf0e26b18f07c8550f
ascot20 <danielotchere18@gmail.com>
2023-03-30 01:52:46 -0700

    fixed requirement

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper-lowercase to uppercase
+ * @s:pointer to string
+ * Return:character
+ */
+
 char *string_toupper(char *s)
 {
-    int i;
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] > 96 && s[i] < 123)
-        {
-            s[i] = s[i] - 32;
-        }
-    }
-    return (s);
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
 }
