2f1048edebd86b18632f16e562ecd1e72eec2e16
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-03-30 09:16:52 +0000

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* cap_string-capitalizes strings
+* @c:pointer to string
+* Return:returns string
+*/
+char *cap_string(char *c)
+{
+    int i;
+
+    for (i = 0; c[i] != '\0'; i++)
+    {
+        if (c[i] == ' ' || c[i] == '\n' || c[i] == '\t' || c[i] == '.')
+        {
+            if (c[i + 1] > 96 && c[i + 1] < 123)
+            {
+                c[i + 1] = c[i + 1] - 32;
+            }
+        }
+    }
+    return (c);
+}
