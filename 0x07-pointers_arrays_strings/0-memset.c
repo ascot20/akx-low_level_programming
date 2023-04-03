67777b9401c1e3d9dc3634c9eafc09f8c86652d1
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-03 11:01:08 +0000

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,12 +4,14 @@
  * @s:pointer to string
  * @b:charater to replace with
  * @n:size
+ * Return:pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 
 	int i;
+	
 	for (i = 0; i < n; i++)
 		s[i] = b;
-	return s;
+	return (s);
 }
