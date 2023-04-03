0025c5a3de358327bb43e04ff3b33f99cf01c7dd
ascot20 <danielotchere18@gmail.com>
2023-04-03 03:59:05 -0700

    added file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy-copies memory area
+ * @dest:pointer to destination
+ * @src:pointer to source
+ * @n:number of bytes
+ * Return:pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*dest = src[i];
+		dest++;
+	}
+	return (dest);
+}
