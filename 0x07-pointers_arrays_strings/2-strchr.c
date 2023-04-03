c8d20c324cb434489e36001fc0fd146b527de13b
ascot20 <danielotchere18@gmail.com>
2023-04-03 04:30:48 -0700

    added file

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strchr-locates a character in a string
+ * @s:pointer to string
+ * @c:character to be located
+ * Return:pointer to s
+ */
+char *_strchr(char *s, char c)
+{
+	do{
+		if (*s == c)
+			return (s);
+		s++;
+	}while(*s);
+	return (NULL);
+}
