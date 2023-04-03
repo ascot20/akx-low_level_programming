eeaed7d235252124fdf39fbdec5124517869a11d
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-03 18:06:33 +0000

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* strspn-gets the lenght of a prefix substring
+* @s:pointer to string
+* @accept:pointer to string
+* Return: unsigned int
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+	unsigned int c = 0;
+	
+	while (s[i])
+	{
+		if (s[i] == '\0')
+			break;
+		if (s[i] == ' ')
+			break;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				c++;
+		}
+		i++;
+	}
+	return c;
+}
