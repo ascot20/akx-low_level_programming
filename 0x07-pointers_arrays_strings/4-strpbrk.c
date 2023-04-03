ef28870895627c3a4ed5070132e779fe551ab2fa
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-03 18:56:52 +0000

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,5 +7,16 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
-
+	while (*s != '\0')
+	{
+		char *temp = accept;
+		while (*temp != '\0')
+		{
+			if (*temp == *s)
+				return (s);
+			temp++;
+		}
+		s++;
+	}
+    return ('\0');
 }
