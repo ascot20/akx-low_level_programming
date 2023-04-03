6fe44382102208daac4cf226303112f3f6f3d9a7
ascot20 <danielotchere18@gmail.com>
2023-04-03 03:31:22 -0700

    added directory,readme and file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset-fills memory with a constant byte
+ * @s:pointer to string
+ * @b:charater to replace with
+ * @n:size
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	int i;
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+directory for pointers arrays and strings
