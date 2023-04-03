963df1bbc7fd52060691b26d0bb261b51c1abb41
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-03 18:23:28 +0000

    Create 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _strpbrk-searches a string for any of a set of string
+* @s:pointer to string
+* @accept:pointer to string
+* Return: char
+*/
+char *_strpbrk(char *s, char *accept)
+{
+
+}
