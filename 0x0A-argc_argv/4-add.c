39c066f8392241f1af54d7c8c23b01a6efb38ad5
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-09 18:17:40 +0000

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		char *arg = argv[i];
 		int j;
 
 		for (j = 0; arg[j] != '\0'; j++)
 		{
-			if (!isdigit(arg[j])
+			if (!isdigit(arg[j]))
 			{
 				printf("Error\n");
 				return (1);
