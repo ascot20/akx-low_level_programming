d2abae1d241632ecdccfa20c49bc1ce29d44e075
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-09 17:56:16 +0000

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,9 +9,14 @@
 */
 int main(int argc, char *argv[])
 {
+	int a;
+	int b;
+	
 	if (argc == 3)
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
 		return (0);
 	}
 	else
