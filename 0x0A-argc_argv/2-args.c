b3717c46de388b126eea82f17646d433b7b1403b
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-09 17:44:03 +0000

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+#include "main.h"
+/**
+* main-entry point
+* @argc:argument counter
+* @argv:argument vector
+*
+* Return:return 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
