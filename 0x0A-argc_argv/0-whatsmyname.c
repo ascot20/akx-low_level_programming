68b971c1f1b5ad14f383061f8739d65015db753e
ascot20 <danielotchere18@gmail.com>
2023-04-09 08:14:10 -0700

    new directory,readme and file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main-entry point
+ * @argc:argument counter
+ * @argv:argument vector
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
