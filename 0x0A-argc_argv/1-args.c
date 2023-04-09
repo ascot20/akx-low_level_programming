a49b7401a8e26706ce2d8b346d8c8171f7a19e76
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-09 17:35:05 +0000

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main-entry point
+* @argc:argument counter
+* @argv:argument vector
+* REturn:return 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
