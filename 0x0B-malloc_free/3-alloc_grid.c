df515ae0d0d48812a859b116203ebc5ce812f5c0
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-11 14:58:27 +0000

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,8 +23,12 @@ int **alloc_grid(int width, int height)
 	{
 		ptr[i] = (int *)malloc(width * sizeof(int));
 		if (ptr[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(ptr[j]);
+			free(ptr)
 			return (NULL);
-
+		}
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
