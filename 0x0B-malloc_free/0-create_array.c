2712e95b8d5f065a56c3dd9378ebfaaf5b722a0b
ascot20 <danielotchere18@gmail.com>
2023-04-11 04:54:55 -0700

    fixed error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,12 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
+	if (ptr == NULL)
+		exit (1);
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
-	
+
 	return (ptr);
 
 }
