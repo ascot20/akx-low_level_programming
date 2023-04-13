e5f3f567ac493141ac41cd76453a3b4e0a8055e7
ascot20 <danielotchere18@gmail.com>
2023-04-13 05:44:48 -0700

    added file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 		return (NULL);
 
-	for(i = 0; min <= max; min++, i++)
+	for (i = 0; min <= max; min++, i++)
 		ptr[i] = min;
 
 	return (ptr);
