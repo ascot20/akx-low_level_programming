d6e523bdd76118100a13e4f0f6a950a909e270c7
ascot20 <danielotchere18@gmail.com>
2023-04-17 12:20:46 -0700

    fixed error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,6 @@ void print_dog(struct dog *d)
 		return;
 
 	(*d).name? printf("Name: %s\n", (*d).name) :  printf("Name: (nil)\n");
-	(*d).age? printf("Age: %f\n", (*d).age) :  printf("Name: (nil)\n");
+	printf("Age: %f\n", (*d).age);
 	(*d).owner? printf("Owner: %s\n", (*d).owner) :  printf("Name: (nil)\n");
 }
