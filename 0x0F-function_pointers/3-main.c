a82091b5223a588529dff8daabfa553a4493710e
Daniel Otchere <65907740+ascot20@users.noreply.github.com>
2023-04-19 14:28:22 +0000

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,21 +10,23 @@
 int main(int argc, char *argv[])
 {
 	int (*get_result)(int, int);
-	int result, a, b;
+	int a, b;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	
+	get_result = get_op_func(argv[2]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	
-	get_result = get_op_func(argv[2]);
 
 	 if (get_result == NULL)
         {
@@ -32,11 +34,7 @@ int main(int argc, char *argv[])
 	        exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	result = get_result(a, b);
-
-	printf("%d\n", result);
+	printf("%d\n", get_result(a, b));
 	
 	return (0);
 }
