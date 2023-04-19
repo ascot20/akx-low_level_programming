f4ae94147bf2a650a8f1d443f4695c7c98fa8cf6
ascot20 <danielotchere18@gmail.com>
2023-04-19 06:13:13 -0700

    added file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main-entry point
+ * @argc:argument counter
+ * @argv:argument vector
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,24 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add-returns the sum
+ * @a:int variable
+ * @b:int variable
+ * Return:return int
+ */
 int op_add(int a, int b)
 {
-
+	return (a + b);
 }
 
+/**
+ * op_sub-returns the subtraction
+ * @a:int variable
+ * @b:int variable
+ * Return:return int
+ */
 int op_sub(int a, int b)
 {
-
+	return (a - b);
 }
 
+/**
+ * op_mul-returns the multiplication
+ * @a:int variable
+ * @b:int variable
+ * Return:return int
+ */
 int op_mul(int a, int b)
 {
-
+	return (a * b);
 }
 
+/**
+ * op_div-returns the division
+ * @a:int variable
+ * @b:int variable
+ * Return:return int
+ */
 int op_div(int a, int b)
 {
-
+	return (a / b);
 }
 
+/**
+ * op_mod-returns the remainder
+ * @a:int variable
+ * @b:int variable
+ * Return:return int
+ */
 int op_mod(int a, int b)
 {
-
+	return (a % b);
 }
