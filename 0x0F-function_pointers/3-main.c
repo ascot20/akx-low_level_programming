77301f2ea64d9b3ccb6d838a79c8fded31bd823c
ascot20 <danielotchere18@gmail.com>
2023-04-19 06:59:27 -0700

    added file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
  * main-entry point
@@ -8,9 +9,34 @@
  */
 int main(int argc, char *argv[])
 {
+	int (*get_result)(int, int);
+	int result, a, b;
+
 	if (argc > 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	if ((*argv[2] == "/" || *argv[2] == "%") && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	
+	get_result = get_op_func(argv[2]);
+
+	 if (get_result == NULL)
+        {
+                printf("Error\n");
+	        exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = get_result(a, b);
+
+	printf("%d\n", result);
+	
+	return (0);
 }
